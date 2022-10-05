b15f85d194c33c3fec3143281dba850d8bb95b0a
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2022-10-05 15:57:50 -0400

    Add post-task questionnaire to all tasks

--- examples/10.ts
+++ examples/10.ts
@@ -76,3 +76,27 @@ function subtract(resVec: number[], currentVec: number[]): number[] {
     orthoVec.push(currentVec[i] - resVec[i]);
   return orthoVec;
 }
+
+/**
+ * Post-Task Activity
+ * ------------------
+ * 1. Please list all the bug(s) you found in the above code. 
+ *     - Specify both the input value(s) AND the erroneous result.
+ *     - If you found multiple bugs, make as many entries as you need.
+ *     - If you found no bugs, skip this question.
+ * 
+ *   Buggy Input : 
+ *   Buggy Output: 
+ * 
+ * 2. Please X the box that describes how confident you are that you 
+ *    accurately found all the bugs in the code above.
+ * 
+ *       1             2             3             4             5 
+ *   Not at all     Slightly      Somewhat      Moderately    Extremely
+ *   Confident      Confident     Confident     Confident     Confident
+ * |------------|--------------|-------------|-------------|-------------|
+ * |            |              |             |             |             |
+ * |------------|--------------|-------------|-------------|-------------|
+ * 
+ * 3. Please save this file, commit to the repo, and request the next task.
+ */
--- examples/3.ts
+++ examples/3.ts
@@ -12,3 +12,27 @@ export function totalDinnerExpenses(dinners: number[], total?: any): number {
   dinners.forEach((item) => (total += item));
   return total;
 }
+
+/**
+ * Post-Task Activity
+ * ------------------
+ * 1. Please list all the bug(s) you found in the above code. 
+ *     - Specify both the input value(s) AND the erroneous result.
+ *     - If you found multiple bugs, make as many entries as you need.
+ *     - If you found no bugs, skip this question.
+ * 
+ *   Buggy Input : 
+ *   Buggy Output: 
+ * 
+ * 2. Please X the box that describes how confident you are that you 
+ *    accurately found all the bugs in the code above.
+ * 
+ *       1             2             3             4             5 
+ *   Not at all     Slightly      Somewhat      Moderately    Extremely
+ *   Confident      Confident     Confident     Confident     Confident
+ * |------------|--------------|-------------|-------------|-------------|
+ * |            |              |             |             |             |
+ * |------------|--------------|-------------|-------------|-------------|
+ * 
+ * 3. Please save this file, commit to the repo, and request the next task.
+ */
